8555a0f20aae96b78dadf427a095a191fbb9076b
micas <miguelbernardinosousa2002@gmail.com>
2024-02-12 00:16:04 +0000

    trying to make containers work together

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# Start MySQL
-service mysql start;
+service mysql start 
 
-# Create database table, if it does not already exists
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
 
-# Create user, if it does not already exists, that can manioulate the database table
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql < db1.sql
 
-# Give user all rights
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+kill $(cat /var/run/mysqld/mysqld.pid)
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
-# Refresh MySQL
-mysql -e "FLUSH PRIVILEGES;"
-
-# Restart MySQL
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+mysqld
