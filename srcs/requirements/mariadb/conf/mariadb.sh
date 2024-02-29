7e17d747876723169dee8a18f292ffbc4c6b737e
micas <miguelbernardinosousa2002@gmail.com>
2024-02-29 12:37:01 +0000

    website connection ok, but sometimes nginx gives bad gateway

---
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
+
+service mariadb start
+
+sleep 1
+
+mysql_secure_installation << END
+
+Y
+$DB_ROOT_PASSWORD
+$DB_ROOT_PASSWORD
+Y
+Y
+Y
+Y
+END
+
+    sleep 1
+    mysql -u root -e "CREATE DATABASE $DB_NAME;"
+    mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
+
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
+    mysql -u root -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+    mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
+
+else
+    sleep 1
+    echo "Database is already configured"
+fi
+
+echo "Database is ready to use."
+
+exec "$@"
---
+++ srcs/requirements/wordpress/conf/wordpress.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /var/www/html
+if [ ! -e "/var/www/html/wp-config.php" ]; then
+	wp cli update --yes --allow-root
+	wp core download --allow-root
+	wp config create --dbname=${DB_NAME} --path=/var/www/html/ --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_HOST} --allow-root
+	wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL} --allow-root
+	wp user create "${WP_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASS}" --role=author --display_name="${DB_USER}" --allow-root
+else
+	echo "installed"
+
+fi
+echo "[Wordpress startup] Starting WordPress fastCGI on port 9000."
+
+exec /usr/sbin/php-fpm8.3 -F -R
